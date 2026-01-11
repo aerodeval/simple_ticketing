d3e6842408094f496a5d90dd9d1fd9cf50b92672
Sydney <sydney223as@gmail.com>
2026-01-11 22:16:15 +0530

    feat: providing session details to frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,14 +4,28 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 
-import { Button, setConfig, frappeRequest, resourcesPlugin } from 'frappe-ui'
+import {
+  Button,
+  setConfig,
+  frappeRequest,
+  resourcesPlugin,
+  createResource
+} from 'frappe-ui'
 
-let app = createApp(App)
+const app = createApp(App)
 
 setConfig('resourceFetcher', frappeRequest)
 
 app.use(router)
 app.use(resourcesPlugin)
-
 app.component('Button', Button)
+
+const userResource = createResource({
+  url: '/api/method/simple_ticketing.api.get_user_data',
+  auto: true,
+  cache: 'current-user'
+})
+
+app.provide('userResource', userResource)
+
 app.mount('#app')
