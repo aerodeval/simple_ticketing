8815ff58475a89ca0e712431cfb1f1bef399c510
Sydney <sydney223as@gmail.com>
2026-01-11 22:17:11 +0530

    feat: enhance ticket details view and add button to mark as closed

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-	<header class="bg-gray-200 p-6 border-b-2 ">
+	<header class="bg-gray-200 p-6 border-b-2">
 		<div class="flex justify-between items-end mt-0">
 			<img alt="Vue logo" class="logo" src="../assets/icon.svg" width="60" height="25 " />
 
@@ -8,9 +8,10 @@
 					<Avatar
 						:shape="'circle'"
 						:ref_for="true"
-						image="https://avatars.githubusercontent.com/u/499550?s=60&v=4"
 						label="EY"
 						size="2xl"
+						v-if="userResource.data"
+						:image="userResource.data.user_image"
 					/>
 				</Dropdown>
 			</div>
@@ -20,7 +21,8 @@
 
 <script setup>
 import { Avatar, Dropdown } from "frappe-ui";
-
+import { inject } from "vue";
+const userResource = inject("userResource");
 
 const actions = [
 	{
--- frontend/src/components/TicketDetails.vue
+++ frontend/src/components/TicketDetails.vue
@@ -3,14 +3,22 @@
     <h1 class="text-xl font-bold">Ticket Details</h1>
     <p class="mt-2">
       Ticket Name: <span class="font-bold">{{ ticketName }}</span>
-      Ticket Description: <span class="font-bold">{{ tickets.doc.purchase }}</span>
+      Ticket Raised on: <span class="font-bold">{{dayjs(tickets.doc.purchase).format('DD/MM/YYYY')  }}</span>
+    <div v-html="tickets.doc.description" class="prose">
+    </div>
     </p>
   </div>
+
+      <Button 
+  @click="tickets.setValue.submit({ status: 'Closed' })">
+        Mark as Closed
+    </Button>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router'
 import { createDocumentResource, Button } from 'frappe-ui'
+import dayjs from 'dayjs'
 
 
 const route = useRoute()
@@ -18,6 +26,10 @@ const ticketName = route.params.name
 let tickets = createDocumentResource({
   doctype: 'Support Tickets',
   name: ticketName,
+   whitelistedMethods: {
+    markAsResolved: 'mark_as_resolved',},
 })
+
+
 
 </script>
